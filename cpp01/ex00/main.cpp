0f03825d9f812d607a8cbbc069d81365965fbacb
Eina Safor Coma Bages <ecoma-ba@student.42barcelona.com>
2025-01-20 18:46:35 +0100

    hehehe he confós heap i stack

--- cpp01/ex00/main.cpp
+++ cpp01/ex00/main.cpp
@@ -6,15 +6,15 @@
 /*   By: ecoma-ba <ecoma-ba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 16:26:21 by ecoma-ba          #+#    #+#             */
-/*   Updated: 2025/01/20 16:29:25 by ecoma-ba         ###   ########.fr       */
+/*   Updated: 2025/01/20 18:45:42 by ecoma-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 int main()
 {
-	Zombie *zombie = newZombie("Stackbie");
-	randomChump("Heapbie");
+	randomChump("Stackbie");
+	Zombie *zombie = newZombie("Heapbie");
 	zombie->announce();
 	delete zombie;
 }
