b3a6dce7d9268398b0a31e6685f9a2ae1f351912
Eina Safor Coma Bages <ecoma-ba@student.42barcelona.com>
2025-01-20 16:30:21 +0100

    header

--- cpp00/ex01/PhoneBook.hpp
+++ cpp00/ex01/PhoneBook.hpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ecoma-ba <ecoma-ba@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/20 16:26:55 by ecoma-ba          #+#    #+#             */
+/*   Updated: 2025/01/20 16:27:00 by ecoma-ba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #pragma once
 #include "Contact.hpp"
 #define MAX_CONTACTS 8
@@ -9,7 +21,7 @@ private:
 	size_t _size;
 
 public:
-	PhoneBook(/* args */);
+	PhoneBook();
 	~PhoneBook();
 	void mainMenu();
 	void newContact();
